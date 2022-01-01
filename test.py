220ffeebd092e29bf89bdca8a8076fa2b5e52511
kevmck451 <93955473+kevmck451@users.noreply.github.com>
2021-12-31 20:20:12 -0600

    Add files via upload

--- test.py
+++ test.py
@@ -36,6 +36,8 @@ while (1==1):
     ExitEffect          = 4            # 0=Random / 1=shrink / 2=zoom out / 3=bounce
     )
 
+  LED.ShowGlowingText(CenterHoriz=True,CenterVert=False,h=0,v=8,Text='BONESAW!!!',RGB=LED.HighYellow,ShadowRGB=LED.ShadowYellow,ZoomFactor= 2,GlowLevels=25,DropShadow=True)
+  time.sleep(3)
 
   LED.ShowTitleScreen(
     BigText='I AM',
