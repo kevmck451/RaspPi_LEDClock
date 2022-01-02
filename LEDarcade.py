2e6c7c36c4eec6d61320f5f3e96c3bc5bb4a2551
kevmck451 <93955473+kevmck451@users.noreply.github.com>
2022-01-02 02:57:37 -0600

    Add files via upload

--- LEDarcade.py
+++ LEDarcade.py
@@ -12312,7 +12312,7 @@ def DisplayDigitalClock(
         CenterHoriz=True,
         CenterVert=True,
         h=17,
-        v=16,
+        v=0,
         hh=12,
         RGB=HighWhite,
         ShadowRGB=ShadowWhite,
@@ -12339,7 +12339,7 @@ def DisplayDigitalClock(
       h = (HatWidth  // 2)  - ((ClockSprite.width * ZoomFactor) // 2) + 1
 
     if (CenterVert == True):
-      v = 16
+      v = 0
 
     # Clock at top, random scrolling animations
     if (ClockStyle == 1):
